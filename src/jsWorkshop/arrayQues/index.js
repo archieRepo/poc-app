57da5df9727e28413109efe85519c315ad7f3038
archieRepo <archiepawan11596@gmail.com>
2025-04-30 19:38:21 +0530

    some array ques

--- src/jsWorkshop/arrayQues/index.js
+++ src/jsWorkshop/arrayQues/index.js
@@ -73,6 +73,84 @@ const plindrom = (str) => {
     return str === str.split('').reverse().join('');
 }
 
-console.log(plindrom('ttett'))
+//console.log(plindrom('ttett'))
 
 
+const arr8 = [56,789,2787,8762];
+
+//find differnce between max and min item or find highest difference between two element
+
+const findMaxDiff = (arr) => {
+    const maxVal = Math.max(...arr);
+    const minVal = Math.min(...arr);
+    console.log("values:", minVal, maxVal);
+    return maxVal - minVal
+
+}
+
+//console.log(findMaxDiff(arr8));
+
+// try these TODO console.log(new Map(arr8));
+
+const getWinnerAmmount = (arr) => {
+    let prize = 0;
+    while(arr.length !== 1) {
+        arr.sort((a,b) => a-b)
+        prize += (arr[arr.length-1] - arr[0]);
+        arr[0] = arr[arr.length-1] - arr[0];
+        arr.pop();
+        console.log(hhi);
+    }
+    return prize;
+}
+
+//console.log(getWinnerAmmount([2,3,4,9]));
+
+
+const findPattern = (arr, randomNum) => {
+    const part = Math.floor(arr.length/randomNum);
+    const pattern = []
+    for(let i=1; i <= part; i++){
+        if(i%2 !== 0){
+            pattern.push(...arr.slice(0,randomNum).sort((a,b) => a-b))
+        }else {
+            pattern.push(...arr.slice(randomNum, arr.length).sort((a,b) => b-a))
+        }
+    }
+    return pattern;
+}
+
+//console.log(findPattern([19,5,6, 28, 8, 10, 27, 56], 3));
+
+
+
+
+
+
+
+function main(input){
+    let max = Math.max(...input);
+
+    let indexOfMax = input.indexOf(max);
+    let min = Math.min(...input.slice(0, indexOfMax));
+
+    let diff = max - min;
+
+    return diff;
+};
+
+console.log(main([5,4,3,8,9,10]));
+
+
+let arrToSort = [278, 277, 89, 88];
+
+
+arrToSort.reduce((acc, curr, i) => {
+    console.log(i);
+    if((i > 0) && (acc[i-1] < curr)){
+        acc.push(curr);
+    }
+    return acc;
+}, [arr[0]]);
+
+console.log(arr);
